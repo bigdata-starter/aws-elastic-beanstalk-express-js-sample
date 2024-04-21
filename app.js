56da2455f538ef6693d0362fb2fb16423d54fef4
experimenting <yedlurisrinu@gmail.com>
2024-04-21 12:20:59 -0700

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Application deployment automation'));
+app.get('/', (req, res) => res.send('Application deployment automation - Pipeline Review Stage Test'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
