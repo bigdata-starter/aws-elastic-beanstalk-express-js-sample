95794de87465d90ccff85a2c7b757fffe0930755
experimenting <yedlurisrinu@gmail.com>
2024-04-21 10:37:45 -0700

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('Application deployment automation'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
